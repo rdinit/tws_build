4d1fb13a1af7af95031b8eeb82284c97aed15145
Egor <28570294+rdinit@users.noreply.github.com>
2026-09-24 12:27:10 +0000

    Replace unsupported rich edit control

--- src/savp/UnitSOVIHelp.pas
+++ src/savp/UnitSOVIHelp.pas
@@ -4,11 +4,11 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ComCtrls;
+  Dialogs, StdCtrls;
 
 type
   TFormSOVIHelp = class(TForm)
-    RichEdit1: TRichEdit;
+    RichEdit1: TMemo;
   private
     { Private declarations }
   public
